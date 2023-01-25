e7881a378a861023ebcb274eedea48e7e04e674b
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-01-25 11:53:10 +0530

    feat: Add prototype UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,68 +1,62 @@
 <script>
     import Title from "../lib/title.svelte";
-    import '../styles/global.css';
+    import "../app.css";
+
+    // import '../styles/global.css';
+    let showMenu = false;
+
+    function toggleNavbar() {
+      showMenu = !showMenu;
+    }
 </script>
 
 <header>
-    <nav>
-        <Title title="The App"></Title>
-        
-        <div class="links">
-            <a href="/">Home</a>
-            <a href="/SignUp">Sign Up</a>
-            <a href="/SignIn">Sign In</a>
+    <div>
+        <div>
+          <nav
+            class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center"
+          >
+            <div class="flex items-center justify-between">
+              <a
+                class="antialiased text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400"
+                href="/home"
+                >Encrypt-Tool
+              </a>
+              <!-- Mobile menu button -->
+              <div on:click={toggleNavbar} 
+                on:keypress={toggleNavbar}      
+              class="flex md:hidden">
+                <button
+                  type="button"
+                  class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+                </button>
+              </div>
+            </div>
+      
+            <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+            <div
+              class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0 {showMenu
+                ? 'flex'
+                : 'hidden'}"
+            >
+              <a class="underline text-gray-800 hover:text-blue-400" href="/home">Home</a>
+              <a class="underline text-gray-800 hover:text-blue-400" href="/Encrypt">Encrypt</a>
+              <a class="underline text-gray-800 hover:text-blue-400" href="/Decrypt">Decrypt</a>
+              <a class="underline text-gray-800 hover:text-blue-400" href="/about">About Us</a>
+            </div>
+          </nav>
         </div>
-    
-    </nav>
-
+      </div>
 </header>
 <main>
     <slot></slot>
 </main>
-<footer>
-    <p>© 2020 Enc Tool</p>
+
+<footer class="fixed inset-x-0 bottom-0">
+  <p class="content-center text-sm text-gray-800">Made with ❤️‍🔥</p>
 </footer>
-
-<style>
-    /* Paragraphs */
-p {
-    font-family: 'Open Sans', sans-serif;
-    font-size: 16px;
-    color: #444;
-    line-height: 1.5;
-    margin-bottom: 20px;
-}
-
-/* Headers */
-header {
-    background-color: #f5f5f5;
-    padding: 20px;
-    text-align: center;
-}
-
-/* Footers */
-footer {
-    background-color: #333;
-    color: #fff;
-    padding: 20px;
-    text-align: center;
-}
-
-footer p {
-    color: rgb(202, 202, 202);
-    margin-bottom: 0;
-}
-
-nav {
-    display: flex;
-    max-width:  960px;
-    margin: 0 auto;
-    align-items: center;
-}
-
-.links {
-    margin-left: auto;
-}
-
-/* TODO */
-</style>
+ 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,45 @@
-<div class="home">
-    <p>TODO</p>
+<script>
+    import "../app.css";
+    let text = '';
+    let key = '';
+    let algorithm = 'aes-256-cbc';
+    let reposArrived=false
+    let responseText=""
+  
+    function encrypt() {
+      // TODO implement fetch
+      responseText="ENCRYPTED TEXT"
+      reposArrived = !reposArrived
+    }
+
+</script>
+
+<div class="my-auto p-16 rounded-lg text-center">
+  <input
+      class="w-full my-8 px-6 py-4 text-xl rounded-lg border-0 focus:ring-2 focus:ring-gray-800 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl shadow-md" 
+      bind:value={text}
+      placeholder="Enter text to encrypt" />
+  <input
+    class="w-full my-8 px-6 py-4 text-xl rounded-lg border-0 focus:ring-2 focus:ring-gray-800 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl shadow-md" 
+    bind:value={key}
+    placeholder="Enter key"
+  />
+  
+  <input bind:value={algorithm}
+    class="w-full my-8 px-6 py-4 text-xl rounded-lg border-0 focus:ring-2 focus:ring-gray-800 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl shadow-md" 
+    placeholder="Enter algorithm" 
+  />  
+  
+  <button 
+    class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
+    on:click={encrypt}>Encrypt</button>
+
+  {#if reposArrived }
+    <input
+      class="w-full my-8 px-6 py-4 text-xl rounded-lg border-0 focus:ring-2 focus:ring-gray-800 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl shadow-md" 
+      bind:value={responseText}
+      placeholder="Response Text Goes here "
+    />
+  {/if}
 </div>
+
