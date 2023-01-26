46ecfcfc7788ebc345e3c991f8b5defc7b37263b
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-01-26 14:29:48 +0530

    fix: fix route links in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,12 @@
 <script>
 	import '../app.css';
-	import { push } from "svelte-spa-router";
-
-	function _encrypt() {
-		push("/Encrypt");
-	}
-
-	function _decrypt() {
-		push("/Decrypt");
-	}
-
 </script>
 
 <div class="my-auto p-16 rounded-lg text-center grid grid-row-2 gap-2">
 
-	<button
-		class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
-		on:click={_encrypt}>Encrypt</button
-	>
-	<button
-		class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
-		on:click={_decrypt}>Decrypt</button
-	>
+	<a class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
+		href="/Encrypt">Encrypt Text</a>
+	<a class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
+		href="/Decrypt">Decrypt Text</a>
 
 </div>
