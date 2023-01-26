7fe896abcfad5149be8343310dbc77d5223f17f2
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-01-26 16:20:16 +0530

    feat: reactive button

--- src/routes/Encrypt/+page.svelte
+++ src/routes/Encrypt/+page.svelte
@@ -29,6 +29,8 @@
 		}).join('');
 		return randomKey;
 	}
+
+	$: completeInfo = text !== '' && key !== '';
 </script>
 
 <div class="my-auto p-16 rounded-lg text-center grid grid-row-2 gap-2">
@@ -62,7 +64,8 @@
 	/>
 
 	<button
-		class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
+		disabled={!completeInfo}
+		class="mx-auto disabled:opacity-50 mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
 		on:click={_encrypt}>Encrypt</button
 	>
 
