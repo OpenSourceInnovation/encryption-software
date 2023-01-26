04be6c5699fe327ee23afadb65e071af842af5b7
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-01-26 14:00:01 +0530

    feat!: remove /api/test

--- src/routes/api/test/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from "@sveltejs/kit";
-
-export async function POST({ request }) {
-    const rawbody = await request.text();
-    const body = JSON.parse(rawbody.toString());
-    const { message, key } = body;
-    return json({ encrypted: message, key });
-}
