8efe6d12e56dcd48ca676e7e68287b398a5cadf5
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-01-26 14:00:46 +0530

    fix: fix up Decrypt

--- src/routes/Decrypt/+page.svelte
+++ src/routes/Decrypt/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../../app.css';
-	import { aes256encrypt } from '../../utils';
+	import { aes256decrypt } from '../../utils';
 
 	let text = '';
 	let key = '';
@@ -8,34 +8,31 @@
 	let reposArrived = false;
 	let responseText = '';
 	let useRandomKey = false;
-	let randomKey = genRandomKey();
+	// let randomKey = genRandomKey();
 
-	function _encrypt() {
-		if (useRandomKey) {
-			key = randomKey;
-		}
-		// aes256encrypt(text, key).then((res) => {
-		// 	responseText = res;
-		// 	reposArrived = true;
-		// });
+	function _decrypt() {
+		aes256decrypt(text, key).then((res) => {
+			responseText = res;
+			reposArrived = true;
+		});
 	}
 
-	function genRandomKey() {
-		// generate a random string of 32 bytes
-		const randomString = crypto.getRandomValues(new Uint8Array(32));
-		// convert it to a hex string
-		const randomKey = Array.from(randomString, (byte) => {
-			return ('0' + (byte & 0xff).toString(16)).slice(-2);
-		}).join('');
-		return randomKey;
-	}
+	// function genRandomKey() {
+	// 	// generate a random string of 32 bytes
+	// 	const randomString = crypto.getRandomValues(new Uint8Array(32));
+	// 	// convert it to a hex string
+	// 	const randomKey = Array.from(randomString, (byte) => {
+	// 		return ('0' + (byte & 0xff).toString(16)).slice(-2);
+	// 	}).join('');
+	// 	return randomKey;
+	// }
 </script>
 
 <div class="my-auto p-16 rounded-lg text-center grid grid-row-2 gap-2">
 	<input
 		class="w-full my-8 px-6 py-4 text-xl rounded-lg border-0 focus:ring-2 focus:ring-gray-800 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl shadow-md"
 		bind:value={text}
-		placeholder="Enter text to Decrypt"
+		placeholder="Enter text to Decrypt ( Only AES-256-CBC cipher )"
 	/>
 	{#if !useRandomKey}
 		<input
@@ -44,15 +41,6 @@
 			placeholder="Enter key"
 		/>
 	{/if}
-
-	{#if useRandomKey}
-		<input
-			class="w-full my-8 px-6 py-4 text-xl rounded-lg border-0 focus:ring-2 focus:ring-gray-800 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl shadow-md"
-			bind:value={randomKey}
-			placeholder="Enter key"
-		/>
-	{/if}
-	<input type="checkbox" bind:value={useRandomKey} />Use Random Key
 
 	<input
 		bind:value={algorithm}
@@ -63,7 +51,7 @@
 
 	<button
 		class="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
-		on:click={_encrypt}>Encrypt</button
+		on:click={_decrypt}>Decrypt</button
 	>
 
 	{#if reposArrived}
